9a8c64ee24bd7f96a2c7a46dc126c0322366d4c8
Bruno Heinrich <bruno.heinrich@superlogica.com>
2018-11-13 07:56:24 -0200

    Finalizando frontend e criando estrutura do app mobile

--- src/routes.js
+++ src/routes.js
@@ -5,10 +5,6 @@ const routes = express.Router();
 const TweetController = require('./controllers/TweetController');
 const LikeController = require('./controllers/LikeController');
 
-// routes.get('/', (req, res) => {
-//   return res.send('Hello World');
-// });
-
 routes.get('/tweets', TweetController.index);
 routes.post('/tweets', TweetController.store);
 
